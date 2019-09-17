a7255c127bcf738c55c12ee09639b61d1be8fa07
Zach Dunn <zfdunn@comcast.net>
2019-09-17 16:57:20 -0400

    updated connection.js with some help from peer

--- config/connection.js
+++ config/connection.js
@@ -9,5 +9,33 @@ var app = express();
 // establishing port
 var PORT = process.env.PORT || 3000;
 // ------------------------------------
-// database connection
+
+// ------------------------------------
+// borrowing code off classmate (start)
+// ------------------------------------
+// database connection variable
 var connection;
+if (process.env.burger_db){
+    connection = mysql.createConnection(process.env.burger_db)
+    }
+    else {
+        connection = mysql.createConnection({
+            host: "localhost",
+            port: 3306,
+            user: "root",
+            password: "password",
+            database: "burger_db"
+        });
+    };
+// ------------------------------------
+    connection.connect(function (err){
+    if (err){
+        console.log("Connection Error: " + err.stack);
+        return;
+    };
+    console.log("Connected as id: " + connection.threadID);
+});
+// ------------------------------------
+// (end)
+
+module.exports = connection;
